b9a858c6aeda67a2907a790cabbc54f9b721a7df
bananafish <bananafish@installgentoo.com>
2014-10-13 18:37:47 +0200

    added my api

---
+++ public/js/config.js
@@ -0,0 +1,12 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm775910525-1da9270f029a769c1f1672a9','m776281518-ab17b6fe033fa91e457baaff','m775918811-31c4fd02aee8dbb0944f32aa','m775925094-e94eb78b6f97b70514426c18','m775918806-38614c5409f4664116d002c9','m776115137-7b3d5e90a169187b4f896386','m776034529-1d4bfdc84ad75a928c4ebb7d','m775978809-7206711e2c8dd6ebf949287e','m775918809-1a9e517dccf700ef5189c02f'
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
+
+// the default language
+// set false to disable language support and show only english,
+// or set null to use visitor's browser language
+var __language = null;
